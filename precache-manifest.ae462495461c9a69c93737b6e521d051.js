ccdb581f29ea2f9a11800b55cf183800667cefdc
Dialest <dialest@hotmail.com>
2019-05-05 23:36:55 -0700

    Updates

---
+++ precache-manifest.ae462495461c9a69c93737b6e521d051.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c6e89a1068af16dfbc8d827077cf70",
+    "url": "/smb3-memory-cards/index.html"
+  },
+  {
+    "revision": "432a2f231061a1b91e0f",
+    "url": "/smb3-memory-cards/static/css/main.4a1663e8.chunk.css"
+  },
+  {
+    "revision": "4e029b16120c0b7539f5",
+    "url": "/smb3-memory-cards/static/js/2.9742547c.chunk.js"
+  },
+  {
+    "revision": "432a2f231061a1b91e0f",
+    "url": "/smb3-memory-cards/static/js/main.341119b9.chunk.js"
+  },
+  {
+    "revision": "eff6f7a16af9e5f8e247",
+    "url": "/smb3-memory-cards/static/js/runtime~main.d6e4a95c.js"
+  },
+  {
+    "revision": "a950c25e2fd5a16541e436b604e29949",
+    "url": "/smb3-memory-cards/static/media/smb3_bgm.a950c25e.mp3"
+  },
+  {
+    "revision": "f1ceaab0b12d39d9bf38b1929cdf4b87",
+    "url": "/smb3-memory-cards/static/media/smb3_flip.f1ceaab0.wav"
+  },
+  {
+    "revision": "17822b91c0b4edf82ec458494cfdd6da",
+    "url": "/smb3-memory-cards/static/media/smb3_match.17822b91.wav"
+  },
+  {
+    "revision": "f44e02650d3ce4644f854ecda4de940c",
+    "url": "/smb3-memory-cards/static/media/smb3_miss.f44e0265.wav"
+  },
+  {
+    "revision": "711608e1534d13ea831479d09fe9cbcf",
+    "url": "/smb3-memory-cards/static/media/smb3_text.711608e1.wav"
+  }
+]);
